8129a1b3ee72b81923209023ef4d94bbc2b0bb5e
b3ll3o <andreazzi-leonardo@hotmail.com>
2019-10-20 19:29:40 -0300

    melhorando a responsividade

--- src/components/Incendios/ListaIncendios/Incendio/ApresentationIncendio/index.js
+++ src/components/Incendios/ListaIncendios/Incendio/ApresentationIncendio/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import { Button, ListGroup } from 'react-bootstrap';
+import { Button, ListGroup, Col, Row } from 'react-bootstrap';
 
 const ApresentationIncendio = ({ incendio, handleClickDelete, toggleUpdate }) => {
 
   return (
     <ListGroup.Item key={incendio._id}>
-      <span>{incendio.latitude.$numberDecimal} - </span>
-      <span>{incendio.longitude.$numberDecimal}</span>
-      <Button variant="warning" onClick={toggleUpdate}>editar</Button>
-      <Button variant="danger" onClick={handleClickDelete}>deletar</Button>
+      <Row>
+        <Col>
+          <span>{incendio.latitude.$numberDecimal}</span>
+        </Col>
+        <Col>
+          <span>{incendio.longitude.$numberDecimal}</span>
+        </Col>
+          <Button variant="warning" onClick={toggleUpdate}>editar</Button>
+          <Button variant="danger" onClick={handleClickDelete}>deletar</Button>
+      </Row>
     </ListGroup.Item>
   );
 }
